dce11e365a879932dce0294ff1f622fd9f4f94d2
kurokonobaskue <luy17@rpi.edu>
2021-10-15 05:51:29 -0400

    Make Profile Page
    Made the profile/user page, plus add images (stock images) as example/filler images. Tried to make the Javascript work, but somehow it isn't linking up with the HTML (or working in general ;-;).

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  $("#sketches").click(function(){
+    $(".clicked").removeClass("clicked");
+    $(".show").addClass("hide")
+    $(".show").removeClass("show");
+
+    $("#sketchesPNG").removeClass("hide");
+    $("#sketches").addClass("clicked")
+    $("#sketchesPNG").addClass("show");
+  });
+
+  $("#commissions").click(function(){
+    $(".clicked").removeClass("clicked");
+    $(".show").addClass("hide")
+    $(".show").removeClass("show");
+
+    $("#commissionsPNG").removeClass("hide");
+    $("#commissions").addClass("clicked")
+    $("#commissionsPNG").addClass("show");
+  });
+
+  $("#gallery").click(function(){
+    $(".clicked").removeClass("clicked");
+    $(".show").addClass("hide")
+    $(".show").removeClass("show");
+
+    $("#galleryPNG").removeClass("hide");
+    $("#gallery").addClass("clicked")
+    $("#galleryPNG").addClass("show");
+  });
+
+});
