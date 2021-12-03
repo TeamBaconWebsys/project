2f793bbd3b38c0f4f3a2ee87d46b3c0641a74168
callumhauber <callumhauber@gmail.com>
2021-12-03 12:53:23 -0500

    add starting database code

---
+++ auth/login.php
@@ -0,0 +1,24 @@
+<!-- Insecure login form -->
+
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>Insecure login form</title>
+</head>
+
+<body>
+  <h4>Please log in</h4>
+  <form action="db.php" method="post">
+    <?php if (isset($_GET['error'])) { ?>
+      <p class="error"><?php echo $_GET['error']; ?></p>
+    <?php } ?>
+    <input type="text" name="username" placeholder="Username"><br />
+    <input type="password" name="password" placeholder="Password"><br />
+    <button name="login" type="submit">Login</button>
+  </form>
+  <p>
+    <a href="register.php">Or, register here</a>
+  </p>
+</body>
+</html>
