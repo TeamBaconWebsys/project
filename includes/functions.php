dd6c8ee8fb17346b44ac146c56d7964fec4a4808
Callum Hauber <callumhauber@gmail.com>
2021-12-06 17:32:50 -0500

    Database refactoring (#22)
    * isolate db connection code
    
    * add a login check fuction
    
    * overhaul login/register pages

---
+++ includes/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// database connection function
+function db_connect() {
+  $db = parse_ini_file("config.ini");
+  $dbhost = $db['dbhost'];
+  $dbname = $db['dbname'];
+  $dbuser = $db['dbuser'];
+  $dbpass = $db['dbpass'];
+  $options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+  return $dbconn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, $options);
+}
+
+?>
