27565e8204f57c0c4d64a0f8989d40b29253be31
luy17 <80281609+luy17@users.noreply.github.com>
2021-12-06 17:45:14 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -29,17 +29,5 @@ $(document).ready(function(){
     $("#gallery").addClass("clicked")
     $("#galleryPNG").addClass("show");
   });
-
-  $.ajax({
-    method:"GET",
-    url: "../posts/post.php",
-    dataType: "json",
-    contentType: "application/json",
-  }).done(function(data) {
-
-    /* the Post Data being displayed + changing the title up
-      Grabbing different items within the JSON that was being sent over.
-    */
-    //var id = data['post'][0]['post_id'];
   
 });
