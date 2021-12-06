6843436aa91385a8751eb14296c263bc6c36553d
callumhauber <callumhauber@gmail.com>
2021-12-06 17:03:47 -0500

    overhaul login/register pages

--- auth/register.php
+++ auth/register.php
@@ -1,23 +1,51 @@
-<!-- Insecure login form -->
-
 <!DOCTYPE html>
+<html lang="en">
 
-<html>
-<head>
-  <title>Insecure login form</title>
-</head>
+  <head>
+    <!-- meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<body>
-  <h4>Please register</h4>
-  <form action="db.php" method="post">
-    <?php if (isset($_GET['error'])) { ?>
-      <p class="error"><?php echo $_GET['error']; ?></p>
-    <?php } ?>
-    <input type="text" name="username" placeholder="Username"><br />
-    <input type="text" name="email" placeholder="Email"><br />
-    <input type="text" name="display_name" placeholder="Display Name (optional)"><br />
-    <input type="password" name="password" placeholder="Password"><br />
-    <button name="register" type="submit">Register</button>
-  </form>
-</body>
+    <!-- Bootstrap 5 stylesheet and JS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous" defer></script>
+
+    <!--custom CSS and JS-->
+    <link rel="stylesheet" href="../assets/style.css">
+    <script src="../assets/script.js" async></script>
+
+    <title>Enter the Soup Kitchen</title>
+  </head>
+
+  <body class="first-time">
+    <a><img src="images/soup_icon.svg" width="145" alt="" class="d-inline-block align-middle mr-2"></a>
+    <h1 class="display-1 text-center">soup.kitchen</h1>
+
+    <div id="wrapper">
+
+      <div class="form_div">
+        <p class="form_label">Enter the Soup Kitchen</p>
+        <form action="db.php" method="post">
+          <?php if (isset($_GET['error'])) { ?>
+          <p class="error">
+            <?php echo $_GET['error']; ?>
+          </p>
+          <?php } ?>
+          <input type="text" name="username" placeholder="Username" class="my-1"><br />
+          <input type="text" name="email" placeholder="Email" class="my-1"><br />
+          <input type="text" name="display_name" placeholder="Display Name (optional)" class="my-1"><br />
+          <input type="password" name="password" placeholder="Password" class="my-1"><br />
+          <button name="register" class="btn btn-warning btn-lg my-1" type="submit">Register</button>
+        </form>
+
+        <p>
+          <a href="login.php" class="btn btn-danger mt-2">Or, login here</a>
+        </p>
+      </div>
+
+    </div>
+  </body>
+
 </html>
--- homepage/foryou.php
+++ homepage/foryou.php
@@ -1,3 +1,7 @@
+<?php
+include('../includes/login_check.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
